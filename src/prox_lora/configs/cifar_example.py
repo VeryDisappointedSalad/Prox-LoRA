e4b9ff6617b4b14fa02ca68c0e4be24f0629b17d
Karol Chojnacki <karcho2001@gmail.com>
2025-12-18 17:52:33 +0100

    ADMM optimizer

--- src/prox_lora/configs/cifar_example.py
+++ src/prox_lora/configs/cifar_example.py
@@ -5,7 +5,9 @@ from prox_lora.infrastructure.trainer import FullTrainConfig, TrainerConfig
 from prox_lora.models.example_cnn import ExampleCNNConfig
 from prox_lora.optimizers.common import OptimizerConfig, SchedulerConfig
 
+import prox_lora.optimizers.ISTA_optimizer
 import prox_lora.optimizers.FISTA_optimizer
+import prox_lora.optimizers.ADMM_optimizer
 
 register_configs(
     FullTrainConfig(
@@ -14,7 +16,7 @@ register_configs(
         dataloader=DataLoaderConfig(batch_size=64, num_workers=4, pin_memory=True),
         model=ExampleCNNConfig(input_shape=(3, 32, 32), hidden_channels=(120, 84), num_classes=10),
         #optimizer=OptimizerConfig(opt="adamw", lr=0.002, weight_decay=1e-5, momentum=0.9),
-        optimizer=OptimizerConfig(opt="fista", lr=0.002, weight_decay=1e-5, momentum=0.9),
+        optimizer=OptimizerConfig(opt="admm", lr=0.002, weight_decay=1e-5, momentum=0.9),
         scheduler=SchedulerConfig(sched="cosine", num_epochs=20, warmup_epochs=1, min_lr=1e-5),
         trainer=TrainerConfig(max_epochs=10, log_every_n_steps=50),
     )
--- src/prox_lora/train.py
+++ src/prox_lora/train.py
@@ -8,7 +8,7 @@ def main() -> None:
     #config = get_config(FullTrainConfig, "mnist_example_FISTA")
     #config = deep_replace(config, {"name": "FISTA_mnist_after_LR_and_config_debug"})
     config = get_config(FullTrainConfig, "cifar_example")
-    config = deep_replace(config, {"name": "cifar-FISTA_prox_lambda=0.001", "dataloader.pin_memory": False})
+    config = deep_replace(config, {"name": "cifar-admm-rho=0.001", "dataloader.pin_memory": False})
     run_training(config, checkpoint_dir=PROJECT_ROOT / "checkpoints")
 
 
